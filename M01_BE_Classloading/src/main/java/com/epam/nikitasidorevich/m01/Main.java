8a9aae0409bbda6b4f6e0b576b4e78fa2b948e17
Nikita_Sidorevich <nikita_sidorevich@epam.com>
2014-10-05 22:38:51 +0300

    Main app sample structure created

--- M01_BE_Classloading/src/main/java/com/epam/nikitasidorevich/m01/Main.java
+++ M01_BE_Classloading/src/main/java/com/epam/nikitasidorevich/m01/Main.java
@@ -1,7 +1,21 @@
 package com.epam.nikitasidorevich.m01;
 
+import com.epam.nikitasidorevich.m01.util.ConfigManager;
+
 public class Main {
-    
+
+    private static void processArgument(String argument) {
+        ConfigManager configManager = ConfigManager.getInstance();
+
+        if(argument.startsWith("-")) {
+            String optionValue = configManager.getProperty(argument);
+            System.out.println(optionValue);
+        }
+    }
+
     public static void main(String[] args) {
-    }    
+        for (String argument : args) {
+            processArgument(argument);
+        }
+    }
 }
---
+++ M01_BE_Classloading/src/main/java/com/epam/nikitasidorevich/m01/util/ConfigManager.java
@@ -0,0 +1,27 @@
+package com.epam.nikitasidorevich.m01.util;
+
+import java.util.ResourceBundle;
+
+/**
+ * Utility class, that extracts from the properties-file (resource bundle)
+ * information, which is necessary for the functioning of the application
+ * Design pattern Singleton used.
+ */
+public class ConfigManager {
+    private static final String BUNDLE_NAME = "com.epam.nikitasidorevich.m01.config";
+
+    private static ConfigManager instance;
+    private ResourceBundle resourceBundle;
+
+    public static synchronized ConfigManager getInstance() {
+        if (instance == null) {
+            instance = new ConfigManager();
+            instance.resourceBundle = ResourceBundle.getBundle(BUNDLE_NAME);
+        }
+        return instance;
+    }
+
+    public String getProperty(String property) {
+        return (String)resourceBundle.getObject(property);
+    }
+}

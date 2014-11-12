85b02466a405a550330ca8747a3093d5aff2f205
Nikita_Sidorevich <nikita_sidorevich@epam.com>
2014-11-13 01:10:27 +0300

    Minor issues fixed

--- BankSystem/bs-service/src/main/java/com/epam/nikitasidorevich/banksystem/account/service/AccountService.java
+++ BankSystem/bs-service/src/main/java/com/epam/nikitasidorevich/banksystem/account/service/AccountService.java
@@ -3,7 +3,9 @@ package com.epam.nikitasidorevich.banksystem.account.service;
 import com.epam.nikitasidorevich.banksystem.account.entity.AccountVO;
 import com.epam.nikitasidorevich.banksystem.exception.ServiceException;
 
+import java.util.List;
+
 public interface AccountService {
 
-    AccountVO fetchAccount(Long bankId, Long personId) throws ServiceException;
+    List<AccountVO> fetchAccounts(Long bankId, Long personId) throws ServiceException;
 }
--- BankSystem/bs-service/src/main/java/com/epam/nikitasidorevich/banksystem/account/service/AccountServiceImpl.java
+++ BankSystem/bs-service/src/main/java/com/epam/nikitasidorevich/banksystem/account/service/AccountServiceImpl.java
@@ -4,14 +4,22 @@ import com.epam.nikitasidorevich.banksystem.account.dao.AccountDAO;
 import com.epam.nikitasidorevich.banksystem.account.dao.AccountDAOImpl;
 import com.epam.nikitasidorevich.banksystem.account.entity.AccountTO;
 import com.epam.nikitasidorevich.banksystem.account.entity.AccountVO;
+import com.epam.nikitasidorevich.banksystem.exception.DAOException;
 import com.epam.nikitasidorevich.banksystem.exception.ServiceException;
+
+import java.util.List;
 
 public class AccountServiceImpl implements AccountService {
 
     @Override
-    public AccountVO fetchAccount(Long bankId, Long personId) throws ServiceException {
+    public List<AccountVO> fetchAccounts(Long bankId, Long personId) throws ServiceException {
         AccountDAO accountDAO = new AccountDAOImpl();
-//        AccountTO accountTO = accountDAO.selectAccounts(bankId)
-        return null;
+        try {
+            List<AccountTO> accountTOs = accountDAO.selectAccounts(bankId, personId);
+            return null;
+        } catch (DAOException e) {
+            throw new ServiceException("Service error", e);
+        }
+
     }
 }
